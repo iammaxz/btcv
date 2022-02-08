1173d7c83973c98f18ce1556a607c5242a1d471b
iammaxz <99248877+iammaxz@users.noreply.github.com>
2022-02-08 14:51:36 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+var CoinKey = require('coinkey');
+var hit = 0;
+var tryN = 0;
+while(hit < 1)
+{
+  var wallet = new CoinKey.createRandom();
+  var address = wallet.publicAddress;
+  var pkey = wallet.privateKey.toString('hex');
+
+  var vanity = address.substring(0,4)
+  console.log(tryN + " " + vanity);
+  if(vanity == "1Max"){
+    console.log(address + " " + pkey);
+    hit = 2
+ }
+ tryN++;
+}
